e3cd76b8815d7d35a972c19b0f1c57ca6728cfb9
kennethmanuelsy <kennethmanuelmataverde.sy@bicol-u.edu.ph>
2024-06-01 20:08:13 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Saverstyles</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <meta charset="UTF-8">
+    <style type="text/css">
+        <!--
+        .style8 {font-size: 24px}
+        .style9 {font-size: 95%; font-weight: bold; color: #003300; font-family: Verdana, Arial, Helvetica, sans-serif; }
+        -->
+    </style>
+</head>
+<body>
+<div id="wrapper">
+
+  <?php
+  include "Header.php";
+  ?>
+
+  <div id="content">
+    <h2><span style="color:#003300"> Welcome To Saverstyles</span></h2>
+    <table width="100%" border="0" cellspacing="3" cellpadding="3">
+      <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td><p><img src="img/Jeans.jpg" alt="box" width="100" height="100" hspace="10" align="left" class="imgleft" title="box" /></p></td>
+        <td><p><img src="img/asd.jpg" alt="box" width="100" height="100" hspace="10" align="left" class="imgleft" title="box" /></p></td>
+        <td><p><img src="img/images.jpg" alt="box" width="100" height="100" hspace="10" align="left" class="imgleft" title="box" /></p></td>
+      </tr>
+      <tr>
+        <td height="26" bgcolor="#BCE0A8"><div align="center" class="style9">Jeans</div></td>
+        <td bgcolor="#BCE0A8"><div align="center" class="style9">Bleasures</div></td>
+        <td bgcolor="#BCE0A8"><div align="center" class="style9">T-Shirts</div></td>
+      </tr>
+    </table>
+    <p>&nbsp;</p>
+  </div>
+ <?php
+ include "Right.php";
+ ?>
+  <div style="clear:both;"></div>
+   <?php
+ include "Footer.php";
+ ?>
+</div>
+</body>
+</html>
